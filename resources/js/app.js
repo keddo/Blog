da51d0f03813e48afabb5311350f6a7d27362c04
unknown <se.kedir.abdurahman@gmail.com>
2018-11-05 12:39:56 +0300

    I have added some frameworks here

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy';
+Vue.use(Buefy);
+
+//Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+
